2c5fe9b8365539001e80052ceabd3db285686d25
aajfr <aajfr@RETRODEV>
2022-05-31 12:18:49 -0400

    Add project files.

---
+++ TerraSharp/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace TerraSharp
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ TerraSharp/Globals/Exceptions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerraSharp.Globals
+{
+    
+    public class TerraSharpException : Exception
+    {
+        private string exceptionName { get; set; }
+        private double exceptionCode { get; set; }
+        private string exceptionMessage { get; set; }
+
+        public TerraSharpException(string name, double code, string message)
+        {
+            this.exceptionName = name;
+            this.exceptionCode = code;
+            this.exceptionMessage = message;
+        }
+        public string exceptionNameGet() => exceptionName;
+        public double exceptionCodeGet() => exceptionCode;
+        public string exceptionMessageGet() => exceptionMessage;
+    }
+    public class Exceptions
+    {
+        protected TerraSharpException AccountTypeInvalidException = new TerraSharpException("AccountTypeInvalidException", 0x80000000,
+            "Account type %s not recognized");
+    }
+}
